4e32aa7dee8771dec027e66bb4fc27659369c350
kirisaki_vk <kirisaki.vk@gmail.com>
2024-06-07 13:49:01 +0300

    chore: NotImplemented exception

---
+++ src/main/java/vk/kirisaki/exceptions/NotImplemented.java
@@ -0,0 +1,11 @@
+package vk.kirisaki.exceptions;
+
+public class NotImplemented extends RuntimeException {
+    public NotImplemented() {
+        super();
+    }
+
+    public NotImplemented(String message) {
+        super(message);
+    }
+}
