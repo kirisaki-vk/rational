30d1dfa8906a372db73b7b9841a9c010ceeef868
kirisaki_vk <kirisaki.vk@gmail.com>
2024-06-07 15:01:57 +0300

    feat: joindre Affirmations

--- src/main/java/vk/kirisaki/affirmation/Affirmation.java
+++ src/main/java/vk/kirisaki/affirmation/Affirmation.java
@@ -1,4 +1,12 @@
 package vk.kirisaki.affirmation;
 
+import vk.kirisaki.conjonction.Conjonction;
+
 public record Affirmation(String valeur, StatutAffirmation statutAffirmation) {
+    public Affirmation joindre(Conjonction conjonction, Affirmation suiteAffirmation) {
+        String nouvelleValeur = valeur + conjonction.valeur + suiteAffirmation.valeur();
+        StatutAffirmation nouvelleSatusAffirmation = conjonction.evaluer(this.statutAffirmation(), suiteAffirmation.statutAffirmation());
+
+        return new Affirmation(nouvelleValeur, nouvelleSatusAffirmation);
+    }
 }
